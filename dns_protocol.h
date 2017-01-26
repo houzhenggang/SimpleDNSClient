66b34b6fc8964bcac3bfd65d0098fde46dc4b048
Kingm0b_ <kingm0b.labs@gmail.com>
2017-01-26 09:29:28 -0200

    Add files via upload

--- dns_protocol.h
+++ dns_protocol.h
@@ -1,30 +1,29 @@
 #include <sys/types.h>
 
-#pragma scalar_storage_order big-endian
+/* Campos do protocolo invertidos para satisfazer big-endian */
 struct dns_header {
 	u_int16_t id;
 
-	u_int16_t qr:1;
-	u_int16_t opcode:4;
-	u_int16_t aa:1;
-	u_int16_t tc:1;
 	u_int16_t rd:1;
+	u_int16_t tc:1;
+	u_int16_t aa:1;
+	u_int16_t opcode:4;
+	u_int16_t qr:1;
 
-	u_int16_t ra:1;
-	u_int16_t z:3;
 	u_int16_t rcode:4;
+	u_int16_t z:3;
+	u_int16_t ra:1;
 
-	u_int16_t qdcount;
-	u_int16_t ancount;
-	u_int16_t nscount;
 	u_int16_t arcount;
+	u_int16_t nscount;
+	u_int16_t ancount;
+	u_int16_t qdcount;
 };
 
 struct dns_question {
-	u_int16_t qtype;
 	u_int16_t qclass;
+	u_int16_t qtype;
 };
-#pragma scalar_storage_order default
 
 struct dns_packet {
 	struct dns_header header;
